6136f0ea8c634811d3952985b5796450e51042cb
Akhilesh kumar <akhileshathkoni79034@gmail.com>
2026-01-13 21:57:47 +0530

    nothing to commit, working tree clean

---
+++ Node js/Lecture 06 - Event Drivian/Calculator/app.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const {requestHandler} = require('./handler');
+
+const server = http.createServer(
+  requestHandler
+);
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`the server running on address http://localhost:${PORT}`);
+})
