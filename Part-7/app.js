a24d1f1e316a0d01b7d910da8c852b39c8f6e664
Akhilesh kumar <akhileshathkoni79034@gmail.com>
2026-01-18 23:37:04 +0530

    update

--- Part-7/app.js
+++ Part-7/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
+const { log } = require('console');
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -8,9 +10,20 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-  res.render('index');
-  // res.send('hello world');
+  fs.readdir('./files', function(err, files){
+    if(err){
+      return res.render('index', { files: [] });
+    }
+    res.render('index', { files: files });
+  });
 });
+
+app.post('/create', function(req, res){
+  fs.writeFile(`./files/$req.body.Title.split(' ').join('_').toLowerCase().txt` , req.body.details, function(err){
+     res.redirect('/');
+  });
+}
+);
 
 app.listen(3000, function(){
   console.log('server started on port 3000');
